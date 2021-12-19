34b393a029359e17dbf066ba9bdfa01b48a806c1
khs <69028828+happysang@users.noreply.github.com>
2021-12-19 21:46:06 +0900

    profile 모델 생성

--- exhbn_project/mainpage/models.py
+++ exhbn_project/mainpage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=10)
+    eng_name = models.CharField(max_length=10)
+    introduce = models.TextField(max_length=100)
+    email = models.EmailField(max_length=20)
+    wpage = models.CharField(max_length=100)
