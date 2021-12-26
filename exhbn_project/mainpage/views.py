ea8437a1b4d01093f41862ffdf6cf9cdfe36b615
Lee SeokGeun <gkwoap@nate.com>
2021-12-27 00:39:32 +0900

    feat: works,work_detail type1,2

--- exhbn_project/mainpage/views.py
+++ exhbn_project/mainpage/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 
 # Create your views here.
 
@@ -10,3 +11,23 @@ def allprofile(request):
 
 def detailprofile(request):
     return render (request, 'detailprofile.html')
+
+
+
+
+def worksall(request): #all
+    works = Work.objects.all()
+    return render (request, 'works.html', {'works': works})
+
+def works(request, wtype): #gd / ad / media
+    new_wtype = Type.objects.get(wtype = wtype)
+    works = Work.objects.filter(wtype = new_wtype)
+    return render (request, 'works.html', {'works': works})
+
+def work_detail(request, pk): # type1
+    work = Work.objects.filter(pk=pk)
+    return render (request, 'work_detail.html', {'work': work})
+
+def work_detail2(request, pk): # type2
+    work = Work.objects.filter(pk=pk)
+    return render (request, 'work_detail2.html', {'work': work})
